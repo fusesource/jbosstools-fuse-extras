130b463315ba2bffe4997e86e2bffac3c93760a0
Andrej Podhradsky <apodhrad@redhat.com>
2017-05-19 13:20:49 +0200

    FUSETOOLS-2121: SAP tooling doesn't add the proper version of camel-sap into pom.xml

--- jboss-fuse-sap-tool-suite/plugins/org.fusesource.ide.sap.ui/src/org/fusesource/ide/sap/ui/editor/provider/SAPVersionDependenciesManager.java
+++ jboss-fuse-sap-tool-suite/plugins/org.fusesource.ide.sap.ui/src/org/fusesource/ide/sap/ui/editor/provider/SAPVersionDependenciesManager.java
@@ -20,20 +20,24 @@ import org.fusesource.ide.camel.editor.provider.ext.IDependenciesManager;
 import org.fusesource.ide.sap.ui.Activator;
 
 public class SAPVersionDependenciesManager implements IDependenciesManager {
-	
-	static final String SAP_VERSION_621 = "6.2.1.redhat-084";
-	static final String SAP_VERSION_630 = "6.3.0.redhat-187";
-	public static final String LAST_SAP_VERSION = SAP_VERSION_630;
+
+	/* Fuse 6.2.1.R7 */
+	static final String SAP_VERSION_621_LATEST = "6.2.1.redhat-183";
+	/* Fuse 6.3.0.R2 */
+	static final String SAP_VERSION_630_LATEST = "6.3.0.redhat-254";
+
+	public static final String LAST_SAP_VERSION = SAP_VERSION_630_LATEST;
+
 	private static Map<String, String> camelToSAPVersionMapping;
 
 	static {
 		camelToSAPVersionMapping = new HashMap<>();
-		camelToSAPVersionMapping.put("2.15.1", SAP_VERSION_621);
-		camelToSAPVersionMapping.put("2.17.0", LAST_SAP_VERSION);
-		camelToSAPVersionMapping.put("2.17.3", LAST_SAP_VERSION);
-		// TODO: update me with every new release of camel and sap supported in tooling
+		camelToSAPVersionMapping.put("2.15.1", SAP_VERSION_621_LATEST);
+		camelToSAPVersionMapping.put("2.17.0", SAP_VERSION_630_LATEST);
+		camelToSAPVersionMapping.put("2.17.3", SAP_VERSION_630_LATEST);
+		// TODO: update me with every new release of camel and sap supported in
+		// tooling
 	}
-	
 
 	public SAPVersionDependenciesManager() {
 		// keep for reflection instanciation
@@ -47,19 +51,33 @@ public class SAPVersionDependenciesManager implements IDependenciesManager {
 	@Override
 	public void updateDependencies(List<Dependency> currentDependencies, String camelVersion) {
 		for (Dependency dependency : currentDependencies) {
-			if(isSAPCamelDependency(dependency)){
+			if (isSAPCamelDependency(dependency)) {
 				dependency.setVersion(computeSapVersion(camelVersion));
 			}
 		}
 	}
 
 	public String computeSapVersion(String camelVersion) {
+		/* Fuse 6.2.1 */
+		if (camelVersion != null && camelVersion.matches("2\\.15\\.1\\.redhat-621[0-9][0-9][0-9]")) {
+			String buildNumber = camelVersion.substring(17);
+			return "6.2.1.redhat-" + buildNumber;
+		}
+
+		/* Fuse 6.3.0 */
+		if (camelVersion != null && camelVersion.matches("2\\.17\\.0\\.redhat-630[0-9][0-9][0-9]")) {
+			String buildNumber = camelVersion.substring(17);
+			return "6.3.0.redhat-" + buildNumber;
+		}
+
+		/* Check default versions */
 		String strippedCamelVersion = camelVersion.replaceAll(".redhat.*", "");
 		return camelToSAPVersionMapping.getOrDefault(strippedCamelVersion, LAST_SAP_VERSION);
 	}
 
 	private boolean isSAPCamelDependency(Dependency dependency) {
-		return Activator.CAMEL_SAP_GROUP_ID.equals(dependency.getGroupId()) && Activator.CAMEL_SAP_ARTIFACT_ID.equals(dependency.getArtifactId());
+		return Activator.CAMEL_SAP_GROUP_ID.equals(dependency.getGroupId())
+				&& Activator.CAMEL_SAP_ARTIFACT_ID.equals(dependency.getArtifactId());
 	}
 
 }
